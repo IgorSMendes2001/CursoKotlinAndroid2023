d929946752212752b8bfc31c7d518f50d3cf3228
IgorSMendes2001 <igor07082001@gmail.com>
2023-12-15 22:07:43 -0300

    Customização do Layout

--- README.md
+++
@@ -1,1 +0,0 @@
-# CursoKotlinAndroid2023
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "devandroid.igor.applistacurso"
+    namespace = "com.igor.applistacurso"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "devandroid.igor.applistacurso"
+        applicationId = "com.igor.applistacurso"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
@@ -41,7 +41,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
